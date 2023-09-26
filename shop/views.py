d4b0ccd687edc71d672467fec5a88129d2c1764b
lens <sviridov1005@gmail.com>
2023-09-26 13:31:59 +0300

    add absolut url

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
