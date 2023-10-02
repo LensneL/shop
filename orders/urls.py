0fa8e490ba4c5e183adfa13140433ebba966b932
lens <sviridov1005@gmail.com>
2023-10-02 11:21:15 +0300

    improved

--- orders/urls.py
+++ orders/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = 'orders'
-
 urlpatterns = [
-
-    path('create/', views.order_create, name='order_create')
+    path('create/', views.order_create, name='order_create'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -14,13 +14,12 @@ def order_create(request):
                 OrderItem.objects.create(order=order,
                                          product=item['product'],
                                          price=item['price'],
-                                         quantity=item['quantity'],
-                                         )
+                                         quantity=item['quantity'])
             cart.clear()
             return render(request, 'orders/order/created.html',
                           {'order': order})
     else:
         form = OrderCreateForm()
-    return render(request, 'orders/order/create.html',
-                  {'cart': cart,
-                   'form': form})
+        return render(request,
+                      'orders/order/create.html',
+                      {'cart': cart, 'form': form})
