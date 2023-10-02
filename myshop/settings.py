6b1ca0d2d2c70faefbd9994d9dfa54b7307a230a
lens <sviridov1005@gmail.com>
2023-10-02 13:21:53 +0300

    add celery and new func(task) order_create(send_mail)

--- myshop/settings.py
+++ myshop/settings.py
@@ -127,3 +127,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ orders/tasks.py
@@ -0,0 +1,18 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from .models import Order
+
+
+@shared_task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subjects = f'Order nr {order.id}'
+    message = f'Dear {order.first_name}, \n\n' \
+              f'You have successfully placed an order.' \
+              f'Your order ID is {order.id}.'
+
+    mail_sent = send_mail(subjects, message,
+                          'Lens@shop.com',
+                          [order.email])
+
+    return mail_sent
