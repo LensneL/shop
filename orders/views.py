90c8bd276120d5ea9198c2906b72582e0b36efb3
lens <sviridov1005@gmail.com>
2023-10-05 10:49:05 +0300

    add redirect for order_create

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
+from django.urls import reverse
 
 
 def order_create(request):
@@ -18,8 +19,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html',
-                          {'order': order})
+            request.session['order_id'] = order.id
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
         return render(request,
