8dae6cbd7a451a9007ed0a89fe548756a34e39d5
lens <sviridov1005@gmail.com>
2023-10-05 11:40:06 +0300

    add new func payment_process

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from decimal import Decimal
+import stripe
+from django.conf import settings
+from orders.models import Order
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_version = settings.STRIPE_API_VERSION
+
+
+def payment_process(request):
+    order_id = request.session.get('order_id', None)
+    order = get_object_or_404(Order, id=order_id)
+    if request.method == 'POST':
+        success_url = request.build_absolute_uri(reverse('payment:completed'))
+        cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
+        session_data = {
+            'mode': 'payment',
+            'client_reference_id': order.id,
+            'success_url': success_url,
+            'cancel_url': cancel_url,
+            'line_items': []
+        }
+        session = stripe.checkout.Session.create(**session_data)
+        return redirect(session.url, code=303)
+    else:
+        return render(request, 'payment/process.html', locals())
