efe6b4f74b62df0c32cf0d8a4ccf896b7d0ecfed
lens <sviridov1005@gmail.com>
2023-10-06 11:09:04 +0300

    improve view payment

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
 from decimal import Decimal
 import stripe
 from django.conf import settings
+from django.shortcuts import render, redirect, reverse, \
+    get_object_or_404
 from orders.models import Order
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -11,9 +12,13 @@ stripe.api_version = settings.STRIPE_API_VERSION
 def payment_process(request):
     order_id = request.session.get('order_id', None)
     order = get_object_or_404(Order, id=order_id)
+
     if request.method == 'POST':
-        success_url = request.build_absolute_uri(reverse('payment:completed'))
-        cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
+        success_url = request.build_absolute_uri(
+            reverse('payment:completed'))
+        cancel_url = request.build_absolute_uri(
+            reverse('payment:canceled'))
+
         session_data = {
             'mode': 'payment',
             'client_reference_id': order.id,
@@ -21,17 +26,23 @@ def payment_process(request):
             'cancel_url': cancel_url,
             'line_items': []
         }
-        for item in order:
-            session_data['line_items'].append(
-                {'price_data': {'unit_amount': int(item.price * Decimal('100')),
-                                'currency': 'usd',
-                                'product_data': {'name': item.product.name}
-                                },
-                 'quantity': item.quantity,
-                 }
-            )
+        # add order items to the Stripe checkout session
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.product.name,
+                    },
+                },
+                'quantity': item.quantity,
+            })
+
         session = stripe.checkout.Session.create(**session_data)
+
         return redirect(session.url, code=303)
+
     else:
         return render(request, 'payment/process.html', locals())
 
