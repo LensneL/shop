60be3d3de3656407bfca70b8ab940cb148bc7222
lens <sviridov1005@gmail.com>
2023-10-06 10:48:34 +0300

    add new funcs pay (completed,canceled) and improve func pay_process

--- payment/views.py
+++ payment/views.py
@@ -21,7 +21,24 @@ def payment_process(request):
             'cancel_url': cancel_url,
             'line_items': []
         }
+        for item in order:
+            session_data['line_items'].append(
+                {'price_data': {'unit_amount': int(item.price * Decimal('100')),
+                                'currency': 'usd',
+                                'product_data': {'name': item.product.name}
+                                },
+                 'quantity': item.quantity,
+                 }
+            )
         session = stripe.checkout.Session.create(**session_data)
         return redirect(session.url, code=303)
     else:
         return render(request, 'payment/process.html', locals())
+
+
+def payment_completed(request):
+    return render(request, 'payment/completed.html')
+
+
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
