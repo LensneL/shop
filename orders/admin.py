b5eab507a280fbc7c025481e89e7de1ac81aa760
lens <sviridov1005@gmail.com>
2023-10-09 14:45:45 +0300

    add new func order_stripe_payment

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe.id:
+        html = f'<a href="{url}" target="_blank"> {obj.stripe.id}</a>'
+        return mark_safe(html)
+    return ''
+
+
+order_stripe_payment.description = 'Stripe payment'
 
 
 @admin.register(Order)
