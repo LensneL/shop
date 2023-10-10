7f31e7dd58f7d2cfc9c94e3aa1e259af33b2b361
lens <sviridov1005@gmail.com>
2023-10-10 13:14:20 +0300

    add new vie order_detail

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def order_create(request):
@@ -26,3 +27,10 @@ def order_create(request):
         return render(request,
                       'orders/order/create.html',
                       {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(id=order_id)
+    return render(request, 'admin/orders/order/detail.html',
+                  {'order': order})
