5da27bc888b23614d634d9d60b0ba82f8971acc4
lens <sviridov1005@gmail.com>
2023-10-13 10:26:40 +0300

    add new func admin_order_pdf

--- orders/views.py
+++ orders/views.py
@@ -43,3 +43,16 @@ def admin_order_detail(request, order_id):
         settings.STATIC_ROOT / 'css.pdf.css'
     )])
     return response
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(
+                                               settings.STATIC_ROOT / 'css/pdf.css')])
+    return response
