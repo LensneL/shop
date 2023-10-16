e0d6b4b67c8ea14ff18c37e979ad56d1373e08b9
lens <sviridov1005@gmail.com>
2023-10-16 13:52:57 +0300

    add coupon from order

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from shop.models import Product
 from django.conf import settings
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+from coupons.models import Coupon
 
 
 class Order(models.Model):
@@ -14,6 +17,12 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ['-created']
