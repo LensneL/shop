a98756f5af2b82870f4b1bade32874bb56734c29
lens <sviridov1005@gmail.com>
2023-10-16 11:09:15 +0300

    add new model coupon

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0),
+                    MaxValueValidator(100)],
+        help_text='Percentage value (0 to 100)'
+    )
+
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.code
